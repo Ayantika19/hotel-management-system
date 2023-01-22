99afeef96b511f117ec821f2647de6de845b0a16
Ayantika19 <ayantikasarkar17@gmail.com>
2023-01-22 09:47:21 -0800

    Updated entities

--- backend/src/main/java/com/hotel/management/repository/HotelManagementHotelRepository.java
+++ backend/src/main/java/com/hotel/management/repository/HotelManagementHotelRepository.java
@@ -1,12 +1,12 @@
-package com.hotel.management.repository;
+// package com.hotel.management.repository;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.hotel.management.model.HotelManagementLocation;
+// import com.hotel.management.model.HotelManagementLocation;
 
-public interface HotelManagementHotelRepository extends JpaRepository<HotelManagementLocation,Long> {
-    List<HotelManagementLocation> getAllLocationNames();
-    List<HotelManagementLocation> addNewLocation(HotelManagementLocation locationName);
-}
+// public interface HotelManagementHotelRepository extends JpaRepository<HotelManagementLocation,Long> {
+//     List<HotelManagementLocation> getAllLocationNames();
+//     List<HotelManagementLocation> addNewLocation(HotelManagementLocation locationName);
+// }
